6197b98434ad5189e94064cd9c8f66c083b8ac26
Aashutosh Dahal <aashutoshdahal91@gmail.com>
2025-12-17 21:46:53 +0545

    yt link added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,25 +7,40 @@ import facebook_icon from "../../assets/facebook_icon.png";
 
 const Footer = () => {
   return (
-    <div className="footer">
-      <div className="footer-icons">
-        <img src={facebook_icon} alt="fb" />
-        <img src={instagram_icon} alt="instagram" />
-        <img src={twitter_icon} alt="twitter" />
-        <img src={youtube_icon} alt="youtube" />
+    <>
+      {/* Fixed YouTube button */}
+      <a 
+        href="https://www.youtube.com/@Aashucode?sub_confirmation=1" 
+        target="_blank" 
+        rel="noopener noreferrer"
+        className="fixed-youtube-btn"
+        title="Subscribe to our YouTube channel"
+      >
+        <img src={youtube_icon} alt="Subscribe on YouTube" />
+      </a>
+
+      <div className="footer">
+        <div className="footer-icons">
+          {/* <img src={facebook_icon} alt="fb" />
+          <img src={instagram_icon} alt="instagram" />
+          <img src={twitter_icon} alt="twitter" /> */}
+         <a href="https://www.youtube.com/@Aashucode?sub_confirmation=1" target="_blank" rel="noopener noreferrer">
+          <img src={youtube_icon} alt="youtube" />
+         </a>
+        </div>
+        <ul>
+          <li>
+            <a
+              href="mailto:fmovieshelp@gmail.com"
+              title="Contact us"
+              style={{ cursor: "pointer", color: "inherit", textDecoration: "none" }}
+            >
+              Contact Us
+            </a>
+          </li>
+        </ul>
       </div>
-      <ul>
-        <li>
-          <a
-            href="mailto:fmovieshelp@gmail.com"
-            title="Contact us"
-            style={{ cursor: "pointer", color: "inherit", textDecoration: "none" }}
-          >
-            Contact Us
-          </a>
-        </li>
-      </ul>
-    </div>
+    </>
   );
 };
 
